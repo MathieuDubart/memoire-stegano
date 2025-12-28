80581ef6a8415b599a59b9f2f07d3270dc786bc8
Mathieu Dubart <mathieu.dubart@icloud.com>
2025-12-28 21:02:01 +0100

    Initial Commit

---
+++ StegApp/StegAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StegAppApp.swift
+//  StegApp
+//
+//  Created by Mathieu Dubart on 28/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct StegAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
